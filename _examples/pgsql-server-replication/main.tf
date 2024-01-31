58f260d24b4c164b9c30dd953ba6a76f620621cb
Deepak Verma <deepak.verma@clouddrove.com>
2024-01-31 23:20:12 +0530

    tfcheck fixed

--- _examples/pgsql-server-replication/main.tf
+++ _examples/pgsql-server-replication/main.tf
@@ -2,43 +2,53 @@ provider "azurerm" {
   features {}
 }
 
-module "resource_group" {
-  source  = ""
-  version = "1.0.0"
-
-  name        = "app-postgresqll2"
-  environment = "test2"
+locals {
+  name        = "app"
+  environment = "test"
   label_order = ["name", "environment"]
+}
+
+##-----------------------------------------------------------------------------
+## Resource Group module call
+## Resource group in which all resources will be deployed.
+##-----------------------------------------------------------------------------
+module "resource_group" {
+  source      = "clouddrove/resource-group/azure"
+  version     = "1.0.2"
+  name        = local.name
+  environment = local.environment
+  label_order = local.label_order
   location    = "Canada Central"
 }
 
+##-----------------------------------------------------------------------------
+## Virtual Network module call.
+##-----------------------------------------------------------------------------
 module "vnet" {
-  source              = ""
-  version             = "1.0.0"
-  name                = "app"
-  environment         = "test2"
-  label_order         = ["name", "environment"]
+  source              = "clouddrove/vnet/azure"
+  version             = "1.0.4"
+  name                = local.name
+  environment         = local.environment
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
-  address_space       = "10.0.0.0/16"
-  enable_ddos_pp      = false
+  address_spaces      = ["10.0.0.0/16"]
 }
 
+##-----------------------------------------------------------------------------
+## Subnet module call.
+##-----------------------------------------------------------------------------
 module "subnet" {
-  source               = ""
-  version              = "1.0.0"
-  name                 = "app"
-  environment          = "test2"
-  label_order          = ["name", "environment"]
+  source               = "clouddrove/subnet/azure"
+  version              = "1.1.0"
+  name                 = local.name
+  environment          = local.environment
   resource_group_name  = module.resource_group.resource_group_name
   location             = module.resource_group.resource_group_location
-  virtual_network_name = join("", module.vnet.vnet_name)
-
+  virtual_network_name = module.vnet.vnet_name
   #subnet
-  default_name_subnet = true
-  subnet_names        = ["default"]
-  subnet_prefixes     = ["10.0.1.0/24"]
-  service_endpoints   = ["Microsoft.Storage"]
+  subnet_names      = ["default"]
+  subnet_prefixes   = ["10.0.1.0/24"]
+  service_endpoints = ["Microsoft.Storage"]
   delegation = {
     flexibleServers_delegation = [
       {
@@ -69,7 +79,7 @@ module "flexible-postgresql" {
   main_rg_name                   = data.azurerm_resource_group.main.name
   resource_group_name            = module.resource_group.resource_group_name
   location                       = module.resource_group.resource_group_location
-  virtual_network_id             = module.vnet.vnet_id[0]
+  virtual_network_id             = module.vnet.vnet_id
   delegated_subnet_id            = module.subnet.default_subnet_id[0]
   postgresql_version             = "12"
   zone                           = "1"
