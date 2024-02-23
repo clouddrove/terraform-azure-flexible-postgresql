92b9e2655a1e8e0afb544a80dae52cd7d1defa6a
Rupal Sharma <rupal.sharma@clouddrove.com>
2024-02-24 00:27:37 +0530

    fix:change in count condition

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "postgresql_flexible_server_id" {
-  value       = azurerm_postgresql_flexible_server.main[0].id
+  value       = try(azurerm_postgresql_flexible_server.main[0].id, null)
   description = "The ID of the PostgreSQL Flexible Server."
 }
 
 output "azurerm_private_dns_zone_virtual_network_link_id" {
-  value       = var.private_dns ? azurerm_private_dns_zone_virtual_network_link.main[0].id : null
+  value       = try(var.private_dns ? azurerm_private_dns_zone_virtual_network_link.main[0].id : null, null)
   description = "The ID of the Private DNS Zone Virtual Network Link."
 }
 
@@ -14,6 +14,6 @@ output "existing_private_dns_zone_virtual_network_link_id" {
 }
 
 output "azurerm_private_dns_zone_id" {
-  value       = var.private_dns ? azurerm_private_dns_zone.main[0].id : null
+  value       = try(var.private_dns ? azurerm_private_dns_zone.main[0].id : null, null)
   description = "The Private DNS Zone ID."
 }
